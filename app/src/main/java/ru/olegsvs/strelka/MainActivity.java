51cee00b4a56d66993fcb1b5c0c104d3ad43c8be
Oleg Sinelnikov <oleg.texet@gmail.com>
2017-03-16 16:35:22 +0300

    Update MainActivity.java

--- app/src/main/java/ru/olegsvs/strelka/MainActivity.java
+++ app/src/main/java/ru/olegsvs/strelka/MainActivity.java
@@ -12,102 +12,83 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import org.json.JSONArray;
 
-public class MainActivity extends Activity
-{
-	private TextView balance;
-	private EditText edStrelkaId;
-    private balanceTask bt;
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		edStrelkaId = (EditText) findViewById(R.id.edStrelkaId);
-		balance = (TextView) findViewById(R.id.tvBalance);
-		balance.setText("");
-		SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-		if (sharedPref.contains("StrelkaIds"))
-		{
-			edStrelkaId.setText(sharedPref.getString("StrelkaIds", ""));
-		}
-	}
-	public void getValues(View v)
-	{
-		if (edStrelkaId.length() < 11)
-		{
-			balance.setText(R.string.hint_11dg);
-			return;
-		}
-		SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-		SharedPreferences.Editor editor = sharedPref.edit();
-		editor.putString("StrelkaIds", edStrelkaId.getText().toString());
-		editor.commit();
-		
-		bt = new balanceTask();
-	    bt.execute();
-	}
+public class MainActivity extends Activity {
+ private TextView balance;
+ private EditText edStrelkaId;
+ private balanceTask bt;
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.main);
+  edStrelkaId = (EditText) findViewById(R.id.edStrelkaId);
+  balance = (TextView) findViewById(R.id.tvBalance);
+  balance.setText("");
+  SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+  if (sharedPref.contains("StrelkaIds")) {
+   edStrelkaId.setText(sharedPref.getString("StrelkaIds", ""));
+  }
+ }
+ public void getValues(View v) {
+  if (edStrelkaId.length() < 11) {
+   balance.setText(R.string.hint_11dg);
+   return;
+  }
+  SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+  SharedPreferences.Editor editor = sharedPref.edit();
+  editor.putString("StrelkaIds", edStrelkaId.getText().toString());
+  editor.commit();
 
-    class balanceTask extends AsyncTask<Void, Void, String>
-	{
-		private String result;
-		@Override
-		protected void onPreExecute()
-		{
-			super.onPreExecute();
-			balance.setText(R.string.loading);
-		}
+  bt = new balanceTask();
+  bt.execute();
+ }
 
-		@Override
-		protected String doInBackground(Void[] p1)
-		{
-			try
-			{
-				URL strelka = new URL("https://strelkacard.ru/api/cards/status/?cardnum=" + edStrelkaId.getText().toString() + "&cardtypeid=3ae427a1-0f17-4524-acb1-a3f50090a8f3");
-				BufferedReader in = new BufferedReader(
-					new InputStreamReader(strelka.openStream()));
+ class balanceTask extends AsyncTask < Void, Void, String > {
+  private String result;
+  @Override
+  protected void onPreExecute() {
+   super.onPreExecute();
+   balance.setText(R.string.loading);
+  }
 
-				String inputLine;
-				StringBuilder sb = new StringBuilder();
+  @Override
+  protected String doInBackground(Void[] p1) {
+   try {
+    URL strelka = new URL("https://strelkacard.ru/api/cards/status/?cardnum=" + edStrelkaId.getText().toString() + "&cardtypeid=3ae427a1-0f17-4524-acb1-a3f50090a8f3");
+    BufferedReader in = new BufferedReader(
+     new InputStreamReader(strelka.openStream()));
 
-				while ((inputLine = in .readLine()) != null)
-					sb.append(inputLine); in .close();
-				if (isJSONValid(sb.toString()))
-				{
-					JSONObject strelkaJSON = new JSONObject(sb.toString());
-					result = (getString(R.string.prBalance) + Double.parseDouble(strelkaJSON.getString("balance")) / 100 + "\u20BD");
-				}
-				else
-					result = getString(R.string.jsonErr);
-			}
+    String inputLine;
+    StringBuilder sb = new StringBuilder();
 
-			catch ( Exception e)
-			{
-				if (e.toString().contains("FileNotFound"))
-					result = getString(R.string.IdNotFnd);
-				if (e.toString().contains("UnknownHost"))
-					result = getString(R.string.internetErr);
-			};
-			return result.toString(); 
-		}
+    while ((inputLine = in .readLine()) != null)
+     sb.append(inputLine); in .close();
+    if (isJSONValid(sb.toString())) {
+     JSONObject strelkaJSON = new JSONObject(sb.toString());
+     result = (getString(R.string.prBalance) + Double.parseDouble(strelkaJSON.getString("balance")) / 100 + "\u20BD");
+    } else
+     result = getString(R.string.jsonErr);
+   } catch (Exception e) {
+    if (e.toString().contains("FileNotFound"))
+     result = getString(R.string.IdNotFnd);
+    if (e.toString().contains("UnknownHost"))
+     result = getString(R.string.internetErr);
+   };
+   return result.toString();
+  }
 
-		@Override
-		protected void onPostExecute(String result)
-		{
-			super.onPostExecute(result);
-			balance.setText(result);
-		}
+  @Override
+  protected void onPostExecute(String result) {
+   super.onPostExecute(result);
+   balance.setText(result);
+  }
 
-		private boolean isJSONValid(String test)
-		{
-			try
-			{
-				new JSONObject(test).getString("balance");
-			}
-			catch (JSONException ex)
-			{
-				return false;
-			}
-			return true;
-		}
-	}
+  private boolean isJSONValid(String test) {
+   try {
+    new JSONObject(test).getString("balance");
+   } catch (JSONException ex) {
+    return false;
+   }
+   return true;
+  }
+ }
 }
