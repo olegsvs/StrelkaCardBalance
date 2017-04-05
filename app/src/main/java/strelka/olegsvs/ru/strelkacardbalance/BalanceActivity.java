4b686e860c719e5a8615cd76e4c5bbc820deb810
olegsvs <oleg.texet@gmail.com>
2017-04-05 20:47:08 +0300

    update

--- app/src/main/java/strelka/olegsvs/ru/strelkacardbalance/BalanceActivity.java
+++ app/src/main/java/strelka/olegsvs/ru/strelkacardbalance/BalanceActivity.java
@@ -1,51 +1,63 @@
-package ru.olegsvs.strelka;
+package strelka.olegsvs.ru.strelkacardbalance;
 
-import android.app.*;
-import android.content.*;
-import android.os.*;
-import android.view.*;
-import android.widget.*;
-import java.io.*;
-import java.net.*;
-import org.json.JSONObject;
-import org.json.JSONException;
-
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.annotation.*;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class BalanceActivity extends AppCompatActivity {
     private TextView balance;
     private EditText edStrelkaId;
     private Button btCheckIt;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE);
+        setContentView(R.layout.activity_balance);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         edStrelkaId = (EditText) findViewById(R.id.edStrelkaId);
         balance = (TextView) findViewById(R.id.tvBalance);
-	    btCheckIt = (Button) findViewById(R.id.btCheckIt);
+        btCheckIt = (Button) findViewById(R.id.btCheckIt);
         final SwipeRefreshLayout layout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
-		layout.setColorSchemeResources(android.R.color.holo_blue_bright,
-			android.R.color.holo_green_light,
-			android.R.color.holo_orange_light,
-			android.R.color.holo_red_light);
-			
-		layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-				@Override
-				public void onRefresh() {
-					layout.postDelayed(new Runnable() {
-						@Override
-						public void run() {
-							layout.setRefreshing(false);
-							getValues(null);
-						}
-					}, 500);
-				}
-			});
-						
+        layout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                layout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        layout.setRefreshing(false);
+                        getValues(null);
+                    }
+                }, 500);
+            }
+        });
+
         ScrollView view = (ScrollView)findViewById(R.id.scView);
         view.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
         view.setFocusable(true);
@@ -64,6 +76,29 @@ public class MainActivity extends Activity {
             balance.setText(R.string.pressMe);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_balance, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void getValues(View v) {
         if (edStrelkaId.length() < 11) {
             balance.setText(R.string.hint_11dg);
@@ -78,7 +113,7 @@ public class MainActivity extends Activity {
         bt.execute();
     }
 
-    private class BalanceTask extends AsyncTask < Void, Void, String > {
+    private class BalanceTask extends AsyncTask< Void, Void, String > {
         private String result, ID;
 
         @Override
@@ -94,7 +129,7 @@ public class MainActivity extends Activity {
             try {
                 URL strelka = new URL("https://strelkacard.ru/api/cards/status/?cardnum=" + ID + "&cardtypeid=3ae427a1-0f17-4524-acb1-a3f50090a8f3");
                 BufferedReader in = new BufferedReader(
-                    new InputStreamReader(strelka.openStream()));
+                        new InputStreamReader(strelka.openStream()));
 
                 String inputLine;
                 StringBuilder sb = new StringBuilder();
@@ -107,10 +142,11 @@ public class MainActivity extends Activity {
                 } else
                     result = getString(R.string.jsonError);
             } catch (Exception e) {
+                result = getString(R.string.Error);
                 if (e.toString().contains("FileNotFound"))
-                    result = getString(R.string.IDNotFound);
+                    Snackbar.make(getWindow().findViewById(android.R.id.content), getString(R.string.IDNotFound), Snackbar.LENGTH_LONG).setAction("Action", null).show();
                 if (e.toString().contains("UnknownHost"))
-                    result = getString(R.string.internetError);
+                    Snackbar.make(getWindow().findViewById(android.R.id.content), getString(R.string.internetError), Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
             return (result != null) ? result : getString(R.string.UnknownError);
         }
